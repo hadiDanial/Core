6dfc6f9b4b4a3876bfb77523fe78e492f6fd2fe6
hadiDanial <hadidanial97@gmail.com>
2021-04-20 00:23:15 +0300

    Created ActionData class

---
+++ Assets/Core/Entity/Actions/ActionData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Core
+{
+    public class ActionData
+    {
+        public Vector3 movementDirection;
+        public bool isDone;
+    }
+}
