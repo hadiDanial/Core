d42c7912480259202708e4c7d43fbf182d74a6a2
hadiDanial <hadidanial97@gmail.com>
2021-04-20 00:24:12 +0300

    Created a Patrol Action
    Created a Patrol action (moved from EnemyAI...)
    Needs testing

---
+++ Assets/Core/Entity/Actions/Patrol.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Core
+{
+    public class Patrol : Action
+    {
+        [SerializeField] public List<Transform> patrolPoints;
+
+        int patrolIndex;
+      
+        internal override IEnumerator PerformAction()
+        {
+            WaitForSeconds wait = new WaitForSeconds(waitTime);
+            while(true)
+            {
+                if (aiDestinationSetter.IsDone())
+                {
+                    patrolIndex++;
+                    patrolIndex = patrolIndex % patrolPoints.Count;
+                    aiDestinationSetter.SetTarget(patrolPoints[patrolIndex]);
+                }
+                data.movementDirection = (patrolPoints[patrolIndex].position - transform.position).normalized;
+                yield return wait;
+            }
+        }
+    }
+}
