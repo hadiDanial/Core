883e300e3dc480ebfc84a2f933bac56d6b7c1ad1
hadiDanial <hadidanial97@gmail.com>
2021-06-10 02:10:43 +0300

    Update Patrol.cs
    Updated to match the new Action format.

--- Assets/Core/Entity/Actions/Patrol.cs
+++ Assets/Core/Entity/Actions/Patrol.cs
@@ -9,11 +9,28 @@ namespace Core.Entities
         [SerializeField] public List<Transform> patrolPoints;
 
         int patrolIndex;
-      
+        public override void StartAction()
+        {
+            base.StartAction();
+            aiDestinationSetter.SetTarget(patrolPoints[patrolIndex]);
+
+        }
+        public override void UpdateAction()
+        {
+            if (aiDestinationSetter.IsDone())
+            {
+                patrolIndex++;
+                patrolIndex = patrolIndex % patrolPoints.Count;
+                aiDestinationSetter.SetTarget(patrolPoints[patrolIndex]);
+                aiDestinationSetter.SetTarget(patrolPoints[patrolIndex]);
+            }
+            data.movementDirection = (patrolPoints[patrolIndex].position - transform.position).normalized;
+        }
+
         protected override IEnumerator PerformAction()
         {
             aiDestinationSetter.SetTarget(patrolPoints[patrolIndex]);
-            WaitForSeconds wait = new WaitForSeconds(waitTime);
+            WaitForSeconds wait = new WaitForSeconds(resetTime);
             while(true)
             {
                 if (aiDestinationSetter.IsDone())
