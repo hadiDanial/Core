d8618a6328d20266db0da0cb9d3cfc16471fd961
hadiDanial <hadidanial97@gmail.com>
2021-06-10 02:09:49 +0300

    Added MoveRigidBody class
    This is the same behavior that was in Entity.Move earlier

---
+++ Assets/Core/Entity/Actions/Movement/MoveRigidbody.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Core.Entities
+{
+    public class MoveRigidbody : MovementAction
+    {
+        public override void UpdateAction()
+        {
+            isGrounded = entity.isGrounded;
+            SetMovementVector(movementVector);
+            //isHittingSide = _isHittingSide;
+            if (entity.IsActive())
+            {
+                //if (isHittingSide)
+                //{
+                //    movementVector.x = 0;
+                //}
+                //jumpTimeElapsed = isGrounded ? jumpGracePeriod : jumpTimeElapsed - Time.deltaTime;
+                if (movementVector != Vector2.zero)
+                {
+                    currentMovementMultiplier = isGrounded ? 1 : airControlPercent;
+                    rb.AddForce(movementVector * totalSpeedMultiplier * Time.deltaTime, ForceMode2D.Force);
+                }
+                else
+                {
+                    rb.AddForce(-rb.velocity.x * Vector2.right * totalSpeedMultiplier * stoppingPower * Time.deltaTime);
+                }
+                if (entity.useGravity)
+                {
+                    rb.gravityScale = (rb.velocity.y < 0f) ? fallGravity : normalGravity;
+                }
+            }
+        }
+
+        public override void Update()
+        {
+
+        }
+
+        public void FixedUpdate()
+        {
+            if (hasStarted && !isPaused)
+                UpdateAction();
+        }
+    }
+}
