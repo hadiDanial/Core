d6526bb03c0d4c8cc15aefafe04368cec1e91327
hadiDanial <hadidanial97@gmail.com>
2021-06-13 02:33:57 +0300

    Update Action.cs

--- Assets/Core/Entity/Actions/Action.cs
+++ Assets/Core/Entity/Actions/Action.cs
@@ -16,19 +16,29 @@ namespace Core.Entities
         protected Entity entity;
         protected Rigidbody2D rb;
         protected bool hasStarted = false, isPaused = true;
-        protected bool alwaysUpdate = false;
+        protected bool alwaysUpdate = false, activeAction = true;
+        private float timer = 0;
 
         public virtual void Initialize(ActionData data)
         {
             this.data = data;
-            aiDestinationSetter = data.aiDestinationSetter;
+            if(data.aiDestinationSetter != null)
+                aiDestinationSetter = data.aiDestinationSetter;
             entity = data.entity;
             rb = entity.rb;
+            entity.health.OnKill += OnEntityDeath;
         }
+
+        private void OnEntityDeath(bool deactivate = true)
+        {
+            StopAction();
+        }
+
         public virtual void Update()
         {
             if (alwaysUpdate || (hasStarted && !isPaused))
                 UpdateAction();
+            timer += Time.deltaTime;
         }
 
         public abstract void UpdateAction();
@@ -38,12 +48,14 @@ namespace Core.Entities
         {
             hasStarted = true;
             isPaused = false;
+            timer = 0;
         }
 
         public virtual void StopAction()
         {
             isPaused = true;
             hasStarted = false;
+            timer = resetTime;
         }
 
         public virtual void Resume()
@@ -62,10 +74,23 @@ namespace Core.Entities
             hasStarted = false;
             Destroy(this);
         }
-
+        protected virtual bool CanPerformAction()
+        {
+            return activeAction && timer >= resetTime && entity != null;
+        }
         protected virtual IEnumerator PerformAction()
         {
             yield return new WaitForSeconds(resetTime);
+        }
+        public void SetActive(bool value)
+        {
+            activeAction = value;
+        }
+
+        private void OnDisable()
+        {
+            entity.health.OnKill -= OnEntityDeath;
+            activeAction = false;
         }
     }
 }
