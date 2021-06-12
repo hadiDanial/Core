40c394db41783fbe4ea3930a11b92a0024dae99d
hadiDanial <hadidanial97@gmail.com>
2021-06-13 02:33:26 +0300

    Update CollisionChecker.cs
    CollisionChecker now tests against a list of check objects.

--- Assets/Core/Entity/CollisionChecker.cs
+++ Assets/Core/Entity/CollisionChecker.cs
@@ -8,19 +8,21 @@ namespace Core
     public class CollisionChecker : MonoBehaviour
     {
         [Header("Ground Check")]
-        [SerializeField] protected GameObject groundCheckLeft;
-        [SerializeField] protected GameObject groundCheckRight;
-        [SerializeField] protected GameObject groundCheckMiddle;
+        [SerializeField] protected List<GameObject> groundChecks = new List<GameObject>();
         [SerializeField] protected LayerMask groundMask;
         internal float groundCheckDistance = 0.1f;
         [Header("Side Check")]
-        [SerializeField] protected GameObject sideCheckLeft;
-        [SerializeField] protected GameObject sideCheckRight;
+        [SerializeField] protected List<GameObject> sideChecksLeft = new List<GameObject>();
+        [SerializeField] protected List<GameObject> sideChecksRight = new List<GameObject>();
         [SerializeField] protected LayerMask wallMask;
         internal float sideCheckDistance = 0.1f;
         [Header("Head Check")]
-        [SerializeField] protected GameObject headCheck;
+        [SerializeField] protected List<GameObject> headChecks = new List<GameObject>();
 
+        List<RaycastHit2D> groundHits = new List<RaycastHit2D>();
+        List<RaycastHit2D> leftHits = new List<RaycastHit2D>();
+        List<RaycastHit2D> rightHits = new List<RaycastHit2D>();
+        List<RaycastHit2D> headHits = new List<RaycastHit2D>();
 
         /// <summary>
         /// Returns true if the Entity is grounded. 
@@ -28,73 +30,112 @@ namespace Core
         /// </summary>
         public bool CheckIsGrounded()
         {
-            if (groundCheckLeft == null || groundCheckRight == null || groundCheckMiddle == null)
+            groundHits.Clear();
+            bool hasHit = false;
+            foreach (GameObject groundCheck in groundChecks)
             {
-                Debug.LogError(transform.name + ": No Ground Check Object!");
-                return false;
+                if (groundCheck == null)
+                {
+                    Debug.LogError(transform.name + ": Null Ground Check Object!");
+                    return false;
+                }
+                else
+                {
+                    RaycastHit2D hit = Physics2D.Raycast(groundCheck.transform.position, Vector2.down, groundCheckDistance, groundMask);
+                    if (hit)
+                        groundHits.Add(hit);
+                    hasHit = hasHit || hit;
+                }
             }
-            RaycastHit2D hitL = Physics2D.Raycast(groundCheckLeft.transform.position, Vector2.down, groundCheckDistance, groundMask);
-            RaycastHit2D hitR = Physics2D.Raycast(groundCheckRight.transform.position, Vector2.down, groundCheckDistance, groundMask);
-            RaycastHit2D hitM = Physics2D.Raycast(groundCheckMiddle.transform.position, Vector2.down, groundCheckDistance, groundMask);
-            if (hitR || hitL || hitM)
-                return true;
-            return false;
+            return hasHit;
         }
 
         public bool CheckIsHittingHead()
         {
-            if (headCheck == null)
+            headHits.Clear();
+            bool hasHit = false;
+            foreach (GameObject headCheck in headChecks)
             {
-                Debug.LogError(transform.name + ": No Head Check Object!");
-                return false;
+                if (headCheck == null)
+                {
+                    Debug.LogError(transform.name + ": No Head Check Object!");
+                    return false;
+                }
+                else
+                {
+                    RaycastHit2D hit = Physics2D.Raycast(headCheck.transform.position, Vector2.up, groundCheckDistance, groundMask);
+                    if (hit)
+                        headHits.Add(hit);
+                    hasHit = hasHit || hit;
+                }
             }
-            RaycastHit2D hit = Physics2D.Raycast(headCheck.transform.position, Vector2.up, groundCheckDistance, groundMask);
-            if (hit)
-                return true;
-            return false;
+            return hasHit;
         }
 
         public bool CheckIsHittingSide(int sign)
         {
-            if (sideCheckLeft == null || sideCheckRight == null)
-            {
-                Debug.LogError(transform.name + ": No Side Check Object!");
-                return false;
-            }
-            GameObject check;
+            leftHits.Clear();
+            rightHits.Clear();
+            List<GameObject> currentSideList;
+            List<RaycastHit2D> currentHits;
+            bool hasHit = false;
             switch (sign)
             {
                 case 1:
-                    check = sideCheckRight;
+                    currentSideList = sideChecksRight;
+                    currentHits = rightHits;
                     break;
-                case 2:
-                    check = sideCheckLeft;
+                case -1:
+                    currentSideList = sideChecksLeft;
+                    currentHits = leftHits;
                     break;
                 default:
-                    check = sideCheckRight;
+                    currentSideList = sideChecksRight;
+                    currentHits = rightHits;
                     break;
             }
-            RaycastHit2D hit = Physics2D.Raycast(check.transform.position, Vector2.right * sign, sideCheckDistance, wallMask);
+            foreach (GameObject sideCheck in currentSideList)
+            {
+                if (sideCheck == null)
+                {
+                    Debug.LogError(transform.name + ": Side Check Object is Null! " + sign);
+                    return false;
+                }
+                RaycastHit2D hit = Physics2D.Raycast(sideCheck.transform.position, Vector2.right * sign, sideCheckDistance, wallMask);
+                if (hit)
+                    currentHits.Add(hit);
+                hasHit = hasHit || hit;
+            }
             // Add walljump check
-            if (hit) return true;
-            return false;
-
+            return hasHit;
         }
 
+        [ExecuteInEditMode]
         private void OnDrawGizmos()
         {
-            if (groundCheckLeft == null || groundCheckRight == null || groundCheckMiddle == null) return;
             Gizmos.color = Color.red;
-            Gizmos.DrawLine(groundCheckRight.transform.position, groundCheckRight.transform.position + Vector3.down * groundCheckDistance);
-            Gizmos.DrawLine(groundCheckLeft.transform.position, groundCheckLeft.transform.position + Vector3.down * groundCheckDistance);
-            if (sideCheckLeft == null || sideCheckRight == null) return;
+            foreach (GameObject groundCheck in groundChecks)
+            {
+                if (groundCheck != null)
+                    Gizmos.DrawLine(groundCheck.transform.position, groundCheck.transform.position + Vector3.down * groundCheckDistance);
+            }
             Gizmos.color = Color.green;
-            Gizmos.DrawLine(sideCheckLeft.transform.position, sideCheckLeft.transform.position + Vector3.left * sideCheckDistance);
-            Gizmos.DrawLine(sideCheckRight.transform.position, sideCheckRight.transform.position + Vector3.right * sideCheckDistance);
-            if (headCheck == null) return;
+            foreach (GameObject sideCheck in sideChecksRight)
+            {
+                if (sideCheck != null)
+                    Gizmos.DrawLine(sideCheck.transform.position, sideCheck.transform.position + Vector3.right * sideCheckDistance);
+            }
+            foreach (GameObject sideCheck in sideChecksLeft)
+            {
+                if (sideCheck != null)
+                    Gizmos.DrawLine(sideCheck.transform.position, sideCheck.transform.position + Vector3.left * sideCheckDistance);
+            }
             Gizmos.color = Color.yellow;
-            Gizmos.DrawLine(headCheck.transform.position, headCheck.transform.position + Vector3.up * groundCheckDistance);
-            Gizmos.DrawLine(groundCheckLeft.transform.position, groundCheckLeft.transform.position + Vector3.down * groundCheckDistance);
+            foreach (GameObject headCheck in headChecks)
+            {
+                if(headCheck != null)
+                    Gizmos.DrawLine(headCheck.transform.position, headCheck.transform.position + Vector3.up * groundCheckDistance);
+            }
         }
     }
 
