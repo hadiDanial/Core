e09878b780f87b0bfd6652d141fa37472a5e436a
hadiDanial <hadidanial97@gmail.com>
2021-06-14 01:49:24 +0300

    JumpMinMax.cs: Fixed buffer jumps

--- Assets/Core/Entity/Actions/Movement/Jump Actions/JumpMinMax.cs
+++ Assets/Core/Entity/Actions/Movement/Jump Actions/JumpMinMax.cs
@@ -9,8 +9,8 @@ namespace Core.Entities
         [SerializeField] protected float minJumpHeight = 1.6f, maxJumpHeight = 4.1f;
         [SerializeField] protected float jumpSpeed = 9.5f;
         [SerializeField] private float verticalSpeed;
-        private float initialHeight, currentHeight;
-        private float finalMinPos, finalMaxPos;
+        protected float initialHeight, currentHeight;
+        protected float finalMinPos, finalMaxPos;
         private bool goingUp = true;
         private bool isBufferJump = false;
 
@@ -22,11 +22,11 @@ namespace Core.Entities
         public override void UpdateAction()
         {
             base.UpdateAction();
-            if (hasStarted && goingUp)
+            if (hasStarted)
             {
                 currentHeight = transform.position.y;
                 if (currentHeight <= finalMinPos || (goingUp && currentHeight < finalMaxPos))
-                    verticalSpeed = jumpSpeed;
+                    verticalSpeed = GetVerticalSpeed();
                 else// if (currentHeight >= finalMaxPos)
                 {
                     StopAction();
@@ -35,17 +35,42 @@ namespace Core.Entities
                 }
                 rb.velocity = new Vector2(rb.velocity.x, verticalSpeed);
             }
+            else if(entity.isGrounded)
+            {
+                if(isWaitingOnBuffer)
+                {
+                    StartAction();
+                }
+            }
         }
 
+        internal virtual float GetVerticalSpeed()
+        {
+            return jumpSpeed;
+        }
+
+        internal override void HandleJumpBuffer()
+        {
+            if (isBufferJump && isWaitingOnBuffer && entity.isGrounded && jumpBufferTimer < jumpBufferPeriod)
+            {
+                jumpBufferTimer = jumpBufferPeriod;
+                StartAction();
+            }
+            else if (jumpBufferTimer > jumpBufferPeriod)
+            {
+                isWaitingOnBuffer = false;
+            }
+        }
         public override void StartAction()
         {
             if (!jumpAllowed || !entity.IsActive())
                 return;
+            base.StartAction();
             if (canJump && !goingUp)
             {
-            base.StartAction();
                 hasJumped = true;
                 goingUp = isBufferJump ? false : true;
+                isBufferJump = false;
                 initialHeight = transform.position.y;
                 finalMinPos = minJumpHeight + initialHeight;
                 finalMaxPos = maxJumpHeight + initialHeight;
