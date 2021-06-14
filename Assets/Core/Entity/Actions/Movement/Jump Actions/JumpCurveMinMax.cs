ba8b592f4cf4f72f191e078fc9ff3da9f1b25c20
hadiDanial <hadidanial97@gmail.com>
2021-06-14 10:52:13 +0300

    Update JumpCurveMinMax.cs

--- Assets/Core/Entity/Actions/Movement/Jump Actions/JumpCurveMinMax.cs
+++ Assets/Core/Entity/Actions/Movement/Jump Actions/JumpCurveMinMax.cs
@@ -9,18 +9,7 @@ namespace Core.Entities
         [SerializeField] private AnimationCurve jumpCurve;
         internal override float GetVerticalSpeed()
         {
-            float b, c;
-            if(initialHeight < 0)
-            {
-                b = finalMaxPos - initialHeight;
-                c = currentHeight - initialHeight;
-            }
-            else
-            {
-                b = finalMaxPos - initialHeight;
-                c = currentHeight - initialHeight;
-
-            }
+            float b = finalMaxPos - initialHeight, c = currentHeight - initialHeight;
             return jumpSpeed * jumpCurve.Evaluate(Mathf.Abs(c / b));
         }
     }
